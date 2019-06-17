4540173a3ae696c1caa1fd1c3c3de38b23bc64c5
solikinspd <51905083+solikinspd@users.noreply.github.com>
2019-06-17 15:34:53 +0700

    Add files via upload

---
+++ safelink_utama.js
@@ -0,0 +1,6 @@
+if(x=="1"){xxx="structured-settlements-do-you-pay-taxes.html"}
+if(x=="2"){xxx="financial-security-through-structured.html"}
+if(x=="3"){xxx="five-reasons-to-home-school-from.html"}
+if(x=="4"){xxx="why-bother-home-schooling.html"}
+if(x=="5"){xxx="auto-insurance-does-not-mean-same.html"}
+if(x=="6"){xxx="secrets-your-auto-insurance-company.html"}a_to_ve[i].href="https://filedukunrpp.blogspot.com/2019/06/ "+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
